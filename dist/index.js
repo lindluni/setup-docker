39cf062510ae7e91a28e230dcafce1578166fff1
Brett Logan <lindluni@github.com>
2022-03-31 14:08:06 -0400

    Update command arguments

--- dist/index.js
+++ dist/index.js
@@ -6184,7 +6184,7 @@ async function validateGroup(name) {
         } else {
             core.info('Docker repository does not exist, downloading')
             await run('echo', [
-                'deb "[arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"',
+                '"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"',
                 '|',
                 'sudo', 'tee', '/etc/apt/sources.list.d/docker.list', '>/dev/null'
             ], true)
